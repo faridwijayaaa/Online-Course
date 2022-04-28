4f7f1feb6174acc74859bda72fe4028f75a1d51a
faridwijayaaa <faridwijayaaa@gmail.com>
2022-04-29 02:34:23 +0700

    Login page

--- routes/pages.js
+++ routes/pages.js
@@ -10,4 +10,12 @@ router.get('/register', (req, res) => {
     res.render('register')
 });
 
+router.get('/index', (req, res) => {
+    res.render('index')
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
 module.exports = router;
